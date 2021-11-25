0cd5879e089bdb20d24625df34acbb2cd44f06ad
avoloshyn10 <avoloshyn.10@gmail.com>
2021-11-25 17:11:31 +0200

    add gameStart()

---
+++ js/game.js
@@ -0,0 +1,28 @@
+/**
+ * Game - main game object
+ *
+ * Copyright (c) 2021 Andrii Voloshyn.
+ * Licensed under the GPL 2 license:
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ */
+
+/*function testev(param) { console.log(param); }*/
+
+var game = new Game();
+
+function Game()
+{
+  this.init = function()
+	{
+
+  }
+}
+
+function gameStart()
+{
+  /*
+  var rng = Math.round(Math.random() * (scenariolist.length - 1))
+  var scenario = "resources/scenarios/xml/" +  scenariolist[rng][0];
+  */
+  game.init();
+}
