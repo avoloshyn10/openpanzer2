b18b659a9aef3a5e99472ef0520187af12437f61
avoloshyn10 <avoloshyn.10@gmail.com>
2021-12-11 14:06:40 +0200

    test xm request

--- js/scenarioloader.js
+++ js/scenarioloader.js
@@ -10,10 +10,26 @@ function ScenarioLoader () {
   let scen = null
   this.loadScenario = function (scenarioObject) {
 		let xmlHttp
-    //
     scen = scenarioObject // ??
     xmlHttp = new XMLHttpRequest() // maybe replace by fetch?
     xmlHttp.open("GET", Scenario.scenarioPath + scen.file) // ,false) // Synchronous XMLHttpRequest on the main thread is deprecated
+    xmlHttp.overrideMimeType('text/xml');
     xmlHttp.send(null)
+    xmlHttp.onload = function() {
+      if (xmlHttp.status != 200) { // анализируем HTTP-статус ответа, если статус не 200, то произошла ошибка
+        console.log(`Ошибка ${xmlHttp.status}: ${xmlHttp.statusText}`); // Например, 404: Not Found
+      } else { // если всё прошло гладко, выводим результат
+        console.log(`Готово, получили ${xmlHttp.response.length} байт`); // response -- это ответ сервера
+      }
+    };
+    //
+    console.log(xmlHttp.responseXML)
+    if ((xmlData = xmlHttp.responseXML) == null) return false
+    console.log('ok')
+    if (!parseMapHeader()) return false
+  }
+
+  function parseMapHeader () {
+
   }
 }
